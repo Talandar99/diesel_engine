9dae3a7fac229640dbd4f13b418732f6afd15929
Talandar <talandar66@protonmail.com>
2026-04-11 17:40:14 +0200

    different types of fuel now have different pollution

--- fuel-value.lua
+++ fuel-value.lua
@@ -1,45 +1,122 @@
 -- force override fuel_value for base fluids
--- fuel_value is calculated based on in game value
+-- force override fuel_value / emissions_multiplier for base fluids
 local overrides = {
 	-------------------------------------------------------
-	--	balanced arround solid fuel
+	-- balanced around solid fuel
 	-------------------------------------------------------
 	-- Vanilla
-	["light-oil"] = "1.2MJ",
-	["heavy-oil"] = "0.6MJ",
-	["petroleum-gas"] = "0.6MJ",
-	["crude-oil"] = "0.3MJ",
+	["light-oil"] = {
+		fuel_value = "1.2MJ",
+		emissions_multiplier = 1.0,
+	},
+	["heavy-oil"] = {
+		fuel_value = "0.6MJ",
+		emissions_multiplier = 2.0,
+	},
+	["petroleum-gas"] = {
+		fuel_value = "0.6MJ",
+		emissions_multiplier = 0.8,
+	},
+	["crude-oil"] = {
+		fuel_value = "0.3MJ",
+		emissions_multiplier = 3.0,
+	},
+
 	-- Pelagos
-	["ethanol"] = "2MJ",
-	["biodiesel"] = "0.8MJ",
-	["methane"] = "0.6MJ",
-	["coconut-oil"] = "0.3MJ",
-	["titanium-sludge"] = "0.15MJ",
+	["ethanol"] = {
+		fuel_value = "2MJ",
+		emissions_multiplier = 0.25,
+	},
+	["biodiesel"] = {
+		fuel_value = "0.8MJ",
+		emissions_multiplier = 0.8,
+	},
+	["methane"] = {
+		fuel_value = "0.6MJ",
+		emissions_multiplier = 0.8,
+	},
+	["coconut-oil"] = {
+		fuel_value = "0.3MJ",
+		emissions_multiplier = 2.0,
+	},
+	["titanium-sludge"] = {
+		fuel_value = "0.15MJ",
+		emissions_multiplier = 2.0,
+	},
+
 	-- Boompuff Agriculture
-	["puff-gas"] = "0.7MJ",
+	["puff-gas"] = {
+		fuel_value = "0.7MJ",
+		emissions_multiplier = 0.8,
+	},
+
 	-- Maraxis, Vesta
-	["hydrogen"] = "0.4MJ",
-	-- Depths of nauvis
-	["uranium-sludge"] = "0.15MJ",
-	-- Spoilded trenches of fulgora
-	["holmium-sludge"] = "0.15MJ",
+	["hydrogen"] = {
+		fuel_value = "0.4MJ",
+		emissions_multiplier = 0.8,
+	},
+
+	-- Depths of Nauvis
+	["uranium-sludge"] = {
+		fuel_value = "0.15MJ",
+		emissions_multiplier = 2.5,
+	},
+
+	-- Spoiled trenches of Fulgora
+	["holmium-sludge"] = {
+		fuel_value = "0.15MJ",
+		emissions_multiplier = 3.0,
+	},
+
 	-- On Wayward Seas
-	["gleba-resin"] = "0.45MJ",
+	["gleba-resin"] = {
+		fuel_value = "0.45MJ",
+		emissions_multiplier = 1.5,
+	},
+
 	-- rocket fuel is fluid
-	["rocket-fuel"] = "1.3MJ",
+	["rocket-fuel"] = {
+		fuel_value = "1.3MJ",
+		emissions_multiplier = 1.5,
+	},
+
 	-- apia-carnova
-	["glycerin"] = "0.25MJ",
+	["glycerin"] = {
+		fuel_value = "0.25MJ",
+		emissions_multiplier = 1.2,
+	},
+
 	-- ferros + hydraulic machines
-	["lubricant"] = "0.10MJ",
+	["lubricant"] = {
+		fuel_value = "0.10MJ",
+		emissions_multiplier = 1.0,
+	},
+	["residual-oil"] = {
+		fuel_value = "0.4MJ",
+		emissions_multiplier = 2.5,
+	},
+	["naphtha"] = {
+		fuel_value = "0.8MJ",
+		emissions_multiplier = 1.1,
+	},
+	["gasoline"] = {
+		fuel_value = "2.0MJ",
+		emissions_multiplier = 1,
+	},
 	-------------------------------------------------------
 }
 
-for name, value in pairs(overrides) do
+for name, values in pairs(overrides) do
 	local fluid = data.raw.fluid[name]
 	if fluid then
-		-- assign value
-		fluid.fuel_value = value
-		-- make sure it's barrelable
+		if values.fuel_value then
+			fluid.fuel_value = values.fuel_value
+		end
+
+		if values.emissions_multiplier then
+			fluid.emissions_multiplier = values.emissions_multiplier
+		end
+
 		fluid.auto_barrel = true
 	end
 end
@@ -47,13 +124,16 @@ end
 if settings.startup["fluid-value-based-flamethrower"].value then
 	local flameturret = data.raw["fluid-turret"]["flamethrower-turret"]
 
-	for fluid_name, fuel_value in pairs(overrides) do
+	for fluid_name, values in pairs(overrides) do
 		local fluid = data.raw.fluid[fluid_name]
-		if fluid then
+		local fuel_value = values.fuel_value
+
+		if fluid and fuel_value then
 			local number_part = util.parse_energy(fuel_value) / 1000000 -- MJ
 
-			if number_part and number_part > 0 then
+			if number_part > 0 then
 				local found = false
+
 				for _, f in pairs(flameturret.attack_parameters.fluids) do
 					if f.type == fluid_name then
 						f.damage_modifier = number_part
@@ -69,7 +149,6 @@ if settings.startup["fluid-value-based-flamethrower"].value then
 					})
 				end
 			end
-		else
 		end
 	end
 end
